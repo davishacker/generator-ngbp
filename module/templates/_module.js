c26e91a799918d9098fb3ca52467ad6bbc5a1429
daihua ye <davishacker86@gmail.com>
2014-06-27 11:11:52 -0700

    update the module for the generator

--- module/templates/_module.js
+++ module/templates/_module.js
@@ -1,26 +1,24 @@
-(function(app) {
-
-    app.config(['$stateProvider', function ($stateProvider) {
-        $stateProvider.state('<%= name %>', {
-            url: '/<%= lowerModuleName %>',
-            views: {
-                "main": {
-                    controller: '<%= capitalModuleName %>Controller',
-                    templateUrl: '<%= name %>/<%= name %>.tpl.html'
-                }
-            },
-            data:{ pageTitle: '<%= capitalModuleName %>' }
-        });
-    }]);
-
-    app.controller('<%= capitalModuleName %>Controller', ['$scope', function ($scope) {
-
-        var init = function() {
-        };
-
-        init();
-    }]);
-
-}(angular.module("<%= projectName %>.<%= camelModuleName %>", [
+angular.module("<%= camelModuleName %>", [
     'ui.router'
-])));
+])
+.config(['$stateProvider', function config ($stateProvider) {
+    $stateProvider.state('<%= name %>', {
+        url: '/<%= lowerModuleName %>',
+        views: {
+            "main": {
+                controller: '<%= capitalModuleName %>Controller',
+                templateUrl: '<%= name %>/<%= name %>.tpl.html'
+            }
+        },
+        data:{ pageTitle: '<%= capitalModuleName %>' }
+    });
+}]);
+
+app.controller('<%= capitalModuleName %>Controller', ['$scope', function ($scope) {
+
+    var init = function() {
+    };
+
+    init();
+}]);
+
